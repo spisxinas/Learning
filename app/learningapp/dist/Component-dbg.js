37723ba45fe03cb64378265873015756e2cc0c4c
Spyridon Pischinas <spischinas@deloitte.gr>
2024-05-31 10:48:02 +0000

    Deployed

---
+++ app/learningapp/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("learninglistreport.learningapp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
